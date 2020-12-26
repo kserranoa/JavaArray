c6a67ac7385b73db6f171560669e027b7f793693
Karina <kserranoa16@gmail.com>
2020-12-26 17:51:46 -0600

    Starting repository with array numbers created

---
+++ javaArray/src/main/java/array/Array.java
@@ -0,0 +1,14 @@
+
+
+package array;
+
+
+public class Array {
+
+    public static void main(String[] args) {
+        // Creando array con numeros
+        int[] numeros = new int[10];
+        
+    }
+
+}
