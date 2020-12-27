7fc233315d5d6f4bc6e999946d5ac2466cc3d431
Karina <kserranoa16@gmail.com>
2020-12-26 20:40:56 -0600

    Replace element and show each element in array

--- javaArray/src/main/java/array/Array.java
+++ javaArray/src/main/java/array/Array.java
@@ -9,9 +9,17 @@ public class Array {
         // Creando array con numeros
         int[] numeros = new int[10];
         
+        int[] numeros2 = {1,2,3,4,5};
+        numeros2[1] =  0;  //reemplazar dato     
+        
+        // Mostrar el arreglo
+        for (int i = 0; i < numeros2.length; i++) {
+                System.out.println(numeros2[i]);
+            }
+        }
         // Creando array con String 
         String[] nombres = new String[5];
         
+        // Convertir frase en lista de palabras
+       
     }
-
-}
